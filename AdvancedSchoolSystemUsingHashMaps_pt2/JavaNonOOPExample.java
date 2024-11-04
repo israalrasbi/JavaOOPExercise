e7aaeefe3752b3a13056c01bd9c68855468a0290
israalrasbi <139228011+israalrasbi@users.noreply.github.com>
2024-11-04 14:01:10 +0400

    third commit

--- AdvancedSchoolSystemUsingHashMaps_pt2/JavaNonOOPExample.java
+++ AdvancedSchoolSystemUsingHashMaps_pt2/JavaNonOOPExample.java
@@ -45,24 +45,25 @@ public class JavaNonOOPExample {
 
     public static void addStudentToSchool(List<HashMap<String, Object>> listOfSchools) {
         System.out.println("Enter school name to add the student to:");
-        //call find school function
+        //call find school function to check if the school exist
         HashMap<String, Object> school = findSchool(listOfSchools, scanner.nextLine());
-        //add elements to the school hashmap
+        //if the school exist, then add elements to the student hashmap
         if (school != null) {
-            HashMap<String, Object> student = new HashMap<>();
+            HashMap<String, Object> studentMap = new HashMap<>();
             System.out.println("Enter student name: ");
-            student.put("student name", scanner.nextLine());
+            studentMap.put("student name", scanner.nextLine());
             System.out.println("Enter student id: ");
-            student.put("student id", scanner.nextLine());
+            studentMap.put("student id", scanner.nextLine());
             System.out.println("Enter student grade: ");
-            student.put("grade",  scanner.nextLine());
+            studentMap.put("grade", scanner.nextLine());
             System.out.println("Enter student age: ");
-            student.put("age", scanner.nextShort());
+            studentMap.put("age", scanner.nextShort());
             scanner.nextLine();
-            student.put("subjects", new ArrayList<HashMap<String, Object>>());
+            studentMap.put("subjects", new ArrayList<HashMap<String, Object>>());
 
-            List<HashMap<String, Object>> students = (List<HashMap<String, Object>>) school.get("students");
-            students.add(student);
+            //add the student hashmap to the list
+            List<HashMap<String, Object>> studentsList = (List<HashMap<String, Object>>) school.get("students");
+            studentsList.add(studentMap);
         } else {
             System.out.println("School not found.");
         }
@@ -70,23 +71,23 @@ public class JavaNonOOPExample {
 
     public static void addSubjectAndMarks(List<HashMap<String, Object>> listOfSchools) {
         System.out.println("Enter school name:");
+        //call find school function to check if the school exist
         HashMap<String, Object> school = findSchool(listOfSchools, scanner.nextLine());
-
         //to add a subject ensure that the school and the student exist
         if (school != null) {
             System.out.println("Enter student ID:");
             //call the find student function
             HashMap<String, Object> student = findStudent((List<HashMap<String, Object>>) school.get("students"), scanner.nextLine());
 
+            //if the there are students, then add elements to subject hashmap
             if (student != null) {
-                //add elements to the subject hashmap
-                HashMap<String, Object> subject = new HashMap<>();
+                HashMap<String, Object> subjectMap = new HashMap<>();
                 System.out.println("Enter subject name: ");
-                subject.put("subject name", scanner.nextLine());
+                subjectMap.put("subject name", scanner.nextLine());
                 System.out.println("Enter teacher name: ");
-                subject.put("teacher name", scanner.nextLine());
+                subjectMap.put("teacher name", scanner.nextLine());
                 System.out.println("Enter credit hours: ");
-                subject.put("credit hours",  scanner.nextShort());
+                subjectMap.put("credit hours", scanner.nextShort());
                 scanner.nextLine();
 
                 List<HashMap<String, Object>> marksList = new ArrayList<>();
@@ -94,21 +95,22 @@ public class JavaNonOOPExample {
                 //take the marks of the subject tests
                 boolean marksFlag = true;
                 while (marksFlag) {
-                    HashMap<String, Object> marks = new HashMap<>();
+                    HashMap<String, Object> marksMap = new HashMap<>();
                     System.out.println("Enter test name:");
-                    marks.put("test name",  scanner.nextLine());
+                    marksMap.put("test name", scanner.nextLine());
                     System.out.println("Enter marks:");
-                    marks.put("marks", scanner.nextInt());
+                    marksMap.put("marks", scanner.nextInt());
                     scanner.nextLine();
-                    marksList.add(marks);
+                    marksList.add(marksMap);
 
+                    //check if there is more marks to enter
                     System.out.println("Add more marks? (yes/no)");
                     marksFlag = scanner.nextLine().equalsIgnoreCase("yes");
                 }
 
-                subject.put("marks", marksList);
+                subjectMap.put("marks", marksList);
                 List<HashMap<String, Object>> subjects = (List<HashMap<String, Object>>) student.get("subjects");
-                subjects.add(subject);
+                subjects.add(subjectMap);
             } else {
                 System.out.println("Student not found.");
             }
@@ -119,18 +121,19 @@ public class JavaNonOOPExample {
 
     public static void retrieveMarks(List<HashMap<String, Object>> listOfSchools) {
         System.out.println("Enter school name:");
-        HashMap<String, Object> school = findSchool(listOfSchools,  scanner.nextLine());
+        HashMap<String, Object> school = findSchool(listOfSchools, scanner.nextLine());
 
         //to retrieve info, ensure that the school and the student id exist
         if (school != null) {
             System.out.println("Enter student ID:");
-            String studentId = scanner.nextLine();
-            HashMap<String, Object> student = findStudent((List<HashMap<String, Object>>) school.get("students"), studentId);
+            HashMap<String, Object> student = findStudent((List<HashMap<String, Object>>) school.get("students"), scanner.nextLine());
 
+            //if the there is a student, then get the subjects
             if (student != null) {
                 System.out.println("Enter subject name:");
-                List<HashMap<String, Object>> subjects = (List<HashMap<String, Object>>) student.get("subjects");
-                for (HashMap<String, Object> subject : subjects) {
+                List<HashMap<String, Object>> subjectsList = (List<HashMap<String, Object>>) student.get("subjects");
+                for (HashMap<String, Object> subject : subjectsList) {
+                    //if the subject match the user input, then print the marks
                     if (subject.get("subject name").equals(scanner.nextLine())) {
                         System.out.println("Marks: " + subject.get("marks"));
                         return;
@@ -147,18 +150,19 @@ public class JavaNonOOPExample {
 
     public static void calculateAverageMarks(List<HashMap<String, Object>> listOfSchools) {
         System.out.println("Enter school name:");
-        HashMap<String, Object> school = findSchool(listOfSchools,  scanner.nextLine());
+        HashMap<String, Object> school = findSchool(listOfSchools, scanner.nextLine());
 
+        //to calculate the marks, check that the school and student exist
         if (school != null) {
             System.out.println("Enter student ID:");
             HashMap<String, Object> student = findStudent((List<HashMap<String, Object>>) school.get("students"), scanner.nextLine());
 
             if (student != null) {
-                List<HashMap<String, Object>> subjects = (List<HashMap<String, Object>>) student.get("subjects");
+                List<HashMap<String, Object>> subjectsList = (List<HashMap<String, Object>>) student.get("subjects");
                 double totalMarks = 0;
                 int count = 0;
 
-                for (HashMap<String, Object> subject : subjects) {
+                for (HashMap<String, Object> subject : subjectsList) {
                     List<HashMap<String, Object>> marksList = (List<HashMap<String, Object>>) subject.get("marks");
                     for (HashMap<String, Object> marks : marksList) {
                         totalMarks += (int) marks.get("marks");
@@ -169,13 +173,13 @@ public class JavaNonOOPExample {
                 if (count > 0) {
                     System.out.println("Average Marks: " + (totalMarks / count));
                 } else {
-                    System.out.println("No marks available.");
+                    System.out.println("No marks available");
                 }
             } else {
-                System.out.println("Student not found.");
+                System.out.println("Student not found");
             }
         } else {
-            System.out.println("School not found.");
+            System.out.println("School not found");
         }
     }
 
