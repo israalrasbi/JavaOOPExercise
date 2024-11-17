6f925ed86d8a54c741d731bb02685f559f231f84
israalrasbi <139228011+israalrasbi@users.noreply.github.com>
2024-11-17 13:06:51 +0400

    created a new class DensityCalculator
    this class will calculate the density and will use a PhysicsData object

---
+++ ScientificEquationCalculator/OOP/Services/DensityCalculator.java
@@ -0,0 +1,12 @@
+package OOP.Services;
+
+import OOP.Entities.PhysicsData;
+import OOP.Interface.CalculatorInterface;
+
+public class DensityCalculator implements CalculatorInterface {
+    private PhysicsData physicsData;
+    @Override
+    public void calculate() {
+
+    }
+}
