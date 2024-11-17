049ab2697a97d98ebe6ed86d383b6417be47f1f4
israalrasbi <139228011+israalrasbi@users.noreply.github.com>
2024-11-17 13:03:39 +0400

    added methods in the ForceCalculator class
    the methods are:
    1- calculateForce()
    2- calculate()

--- ScientificEquationCalculator/OOP/Services/ForceCalculator.java
+++ ScientificEquationCalculator/OOP/Services/ForceCalculator.java
@@ -1,11 +1,43 @@
 package OOP.Services;
 
+import OOP.Entities.PhysicsData;
 import OOP.Interface.CalculatorInterface;
+
+import java.util.Scanner;
 
 public class ForceCalculator implements CalculatorInterface {
     //this class will use PhysicsData
+    private PhysicsData physicsData;
+
+    public double calculateForce() {
+        double m = physicsData.getMass();
+        double a = physicsData.getAcceleration();
+        return m * a;
+    }
+
     @Override
     public void calculate() {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
 
+        while (!exit) {
+            System.out.println("\n--- Force Calculator Menu ---");
+            System.out.println("1. Calculate Force (F = ma)");
+            System.out.println("2. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Force: " + calculateForce());
+                    break;
+                case 2:
+                    exit = true;
+                    System.out.println("Exiting Force Calculator.");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
 }
