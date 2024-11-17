70495c1b73cd79f6f661b899291a58344bb0972f
israalrasbi <139228011+israalrasbi@users.noreply.github.com>
2024-11-17 13:14:25 +0400

    added volume variable with its getters and setters

--- ScientificEquationCalculator/OOP/Entities/PhysicsData.java
+++ ScientificEquationCalculator/OOP/Entities/PhysicsData.java
@@ -4,6 +4,15 @@ public class PhysicsData {
     private double mass;
     private double acceleration;
     private MotionData motionData;
+    private double volume;
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
 
     public double getMass() {
         return mass;
