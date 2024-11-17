b64c673c982ced7cf4d9488d60f1c6a472208037
israalrasbi <139228011+israalrasbi@users.noreply.github.com>
2024-11-17 13:15:52 +0400

    added methods to DensityCalculator class
    the methods are:
    1- calculateDensity()
    2- calculate()

--- ScientificEquationCalculator/OOP/Services/DensityCalculator.java
+++ ScientificEquationCalculator/OOP/Services/DensityCalculator.java
@@ -3,10 +3,40 @@ package OOP.Services;
 import OOP.Entities.PhysicsData;
 import OOP.Interface.CalculatorInterface;
 
+import java.util.Scanner;
+
 public class DensityCalculator implements CalculatorInterface {
     private PhysicsData physicsData;
+
+    public double calculateDensity() {
+        double mass = physicsData.getMass();
+        double volume = physicsData.getVolume();
+        return mass / volume;
+    }
+
     @Override
     public void calculate() {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
 
+        while (!exit) {
+            System.out.println("\n--- Density Calculator Menu ---");
+            System.out.println("1. Calculate Density (Density = Mass ÷ Volume)");
+            System.out.println("2. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Density: " + calculateDensity());
+                    break;
+                case 2:
+                    exit = true;
+                    System.out.println("Exiting Density Calculator.");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
 }
