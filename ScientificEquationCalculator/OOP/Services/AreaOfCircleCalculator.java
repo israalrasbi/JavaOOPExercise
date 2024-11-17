91ceef254cd926305d1ccd3f156d4d5ba30f1a9c
israalrasbi <139228011+israalrasbi@users.noreply.github.com>
2024-11-17 12:51:29 +0400

    added methods to AreaOfCircleCalculator class
    the methods are:
    1- calculateAreaOfCircle()
    2- calculate()

--- ScientificEquationCalculator/OOP/Services/AreaOfCircleCalculator.java
+++ ScientificEquationCalculator/OOP/Services/AreaOfCircleCalculator.java
@@ -1,11 +1,43 @@
 package OOP.Services;
 
+import OOP.Entities.CircleData;
 import OOP.Interface.CalculatorInterface;
+
+import java.util.Scanner;
 
 public class AreaOfCircleCalculator implements CalculatorInterface {
     //this class will use CircleData
+    private CircleData circleData;
+
+    public double calculateAreaOfCircle(){
+        double PI = Math.PI;
+        double radius = circleData.getRadius();
+        return PI*radius;
+    }
+    
     @Override
     public void calculate() {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
 
+        while (!exit) {
+            System.out.println("\n--- Area Of A Circle Calculator Menu ---");
+            System.out.println("1. Calculate  Area Of A Circle (A = πr²)");
+            System.out.println("2. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println(" Area Of A Circle: " + calculateAreaOfCircle());
+                    break;
+                case 2:
+                    exit = true;
+                    System.out.println("Exiting  Area Of A Circle Calculator.");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
 }
